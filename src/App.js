dbbff44a4045eb6cd4c09314d32b0ad2090b8451
ngoc rog ubuntu <maxdota.developer@gmail.com>
2024-11-07 02:18:39 +0700

    setup github pages 3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route} from "react-router-dom";
 import About from "./routes/About";
-import Careers from "./routes/Careers";
 import Home from "./routes/Home";
 import Navbar from './Navbar';
 
